a648737501e75c8f2367b4542ddabf5ad6d13cbc
jdrodriguez <jdrodriguezr@ut.edu.co>
2025-03-10 12:45:22 -0500

    Optimizacion transaccionesDeo para depositos

--- src/DAO/TransaccionDao.java
+++ src/DAO/TransaccionDao.java
@@ -8,13 +8,12 @@ import java.sql.*;
 
 public class TransaccionDao {
 
+    //DEPOSITAR DINERO
     public boolean Depositar(Transaccion transaccion) {
-
         String sqlTransaccion  = "insert into transaccion (num_cuenta, cuenta_destino, tipo, monto, fecha, descripcion) values (?,?,?,?,?,?)";
         String sqlActualizarSaldo  = "update cuenta set saldo = saldo + ? where num_cuenta = ?";
 
         try(Connection cn = ConexionBD.conectar()) {
-
             cn.setAutoCommit(false);
 
             try(PreparedStatement pstTransaccion = cn.prepareStatement(sqlTransaccion);
@@ -29,30 +28,35 @@ public class TransaccionDao {
 
                 int filasAfectadas = pstTransaccion.executeUpdate();
 
-                if (filasAfectadas > 0) {
-
-                    pstSaldo.setInt(1, transaccion.getMonto());
-                    pstSaldo.setInt(2, transaccion.getNum_cuenta());
-
-                    int saldoActualizado = pstSaldo.executeUpdate();
-
-                    if (saldoActualizado > 0) {
-                        cn.commit();
-                        return true;
-                    }
+                if (filasAfectadas == 0) {
+                    cn.rollback();
+                    return false;
                 }
+
+                pstSaldo.setInt(1, transaccion.getMonto());
+                pstSaldo.setInt(2, transaccion.getNum_cuenta());
+
+                int saldoActualizado = pstSaldo.executeUpdate();
+
+                if (saldoActualizado == 0) {
+                    cn.rollback(); //RESTABLECER CAMBIOS
+                    return false;
+                }
+
+                cn.commit(); //CONFIRMAR CAMBIOS
+                return true;
+            }catch(SQLException ex){
+                cn.rollback();
+                System.err.println("Error en el deposito: " + ex.getMessage());
+                JOptionPane.showMessageDialog(null, "Fallo en el deposito, llame a soporte.");
             }
-
-            cn.rollback();
-            return false;
-
         }catch (SQLException ex){
-            JOptionPane.showMessageDialog(null, "Fallo de transaccion, llame a soporte" + ex);
-            System.out.println("Error en la transacción: " + ex.getMessage());
+            System.err.println("Error de conexión: " + ex.getMessage());
         }
         return false;
     }
 
+    //CONSULTAR DINERO
     public Integer ConsultarSaldo(int num_cuenta) {
         String sqlConsultar = "select saldo from cuenta where num_cuenta = ?";
 
@@ -66,13 +70,14 @@ public class TransaccionDao {
                     return rs.getInt("saldo");
                 }
             }
-
         }catch (SQLException ex){
             System.out.println("Error al consultar saldo " + ex.getMessage());
+            return -1;
         }
-        return null;
+        return -1;
     }
 
+    //TRANFERIR DINERO
     public boolean Transferir(Transaccion transaccion){
         String sqlTransaccion  = "insert into transaccion (num_cuenta, cuenta_destino, tipo, monto, fecha, descripcion) values (?,?,?,?,?,?)";
         String sqlRetiroMonto = "update cuenta set saldo = saldo - ? where num_cuenta = ?";
@@ -84,8 +89,30 @@ public class TransaccionDao {
 
             try(PreparedStatement pstTransaccionEnvio = cn.prepareStatement(sqlTransaccion);
                 PreparedStatement pstTransaccionDestino = cn.prepareStatement(sqlTransaccion);
-                PreparedStatement pstInsertar = cn.prepareStatement(sqlCuentaTransferir);
-                PreparedStatement pstRetirar = cn.prepareStatement(sqlRetiroMonto)){
+                PreparedStatement pstCuentaDestino = cn.prepareStatement(sqlCuentaTransferir);
+                PreparedStatement pstCuentaEnvio = cn.prepareStatement(sqlRetiroMonto)){
+
+                //RETIRAR DE CUENTA ENVIO
+                pstCuentaEnvio.setInt(1, transaccion.getMonto());
+                pstCuentaEnvio.setInt(2, transaccion.getNum_cuenta());
+
+                int DineroEnviado = pstCuentaEnvio.executeUpdate();
+
+                if (DineroEnviado == 0) {
+                    cn.rollback();
+                    return false;
+                }
+
+                //DEPOSITAR EN CUENTA DESTINO
+                pstCuentaDestino.setInt(1, transaccion.getMonto());
+                pstCuentaDestino.setInt(2, transaccion.getCuentaDestino());
+
+                int DineroRecibido = pstCuentaDestino.executeUpdate();
+
+                if (DineroRecibido == 0){
+                    cn.rollback();
+                    return false;
+                }
 
                 pstTransaccionEnvio.setInt(1, transaccion.getNum_cuenta());
                 pstTransaccionEnvio.setObject(2, transaccion.getCuentaDestino() == 0 ? null : transaccion.getCuentaDestino(), java.sql.Types.INTEGER);
@@ -94,46 +121,37 @@ public class TransaccionDao {
                 pstTransaccionEnvio.setString(5, transaccion.getFecha());
                 pstTransaccionEnvio.setObject(6, transaccion.getDescripcion() == null ? null : transaccion.getDescripcion(), java.sql.Types.VARCHAR);
 
-                int Envio = pstTransaccionEnvio.executeUpdate();
+                int HistorialEnvio = pstTransaccionEnvio.executeUpdate();
 
-                if (Envio > 0) {
-
-                    pstInsertar.setInt(1, transaccion.getMonto());
-                    pstInsertar.setInt(2, transaccion.getCuentaDestino());
-
-                    int saldoActualizado = pstInsertar.executeUpdate();
-
-                    if (saldoActualizado > 0) {
-                        pstRetirar.setInt(1, transaccion.getMonto());
-                        pstRetirar.setInt(2, transaccion.getNum_cuenta());
-
-                        int saldoRetirado = pstRetirar.executeUpdate();
-
-                        if (saldoRetirado > 0) {
-                            pstTransaccionDestino.setInt(1, transaccion.getCuentaDestino());
-                            pstTransaccionDestino.setObject(2, transaccion.getNum_cuenta() == 0 ? null : transaccion.getNum_cuenta(), java.sql.Types.INTEGER);
-                            pstTransaccionDestino.setString(3, transaccion.getTipo_entrega());
-                            pstTransaccionDestino.setInt(4, transaccion.getMonto());
-                            pstTransaccionDestino.setString(5, transaccion.getFecha());
-                            pstTransaccionDestino.setObject(6, transaccion.getDescripcion() == null ? null : transaccion.getDescripcion(), java.sql.Types.VARCHAR);
-
-                            int Recibido = pstTransaccionDestino.executeUpdate();
-
-                            if (Recibido > 0){
-                                cn.commit();
-                                return true;
-                            }
-                        }
-                    }
+                if (HistorialEnvio == 0) {
+                    cn.rollback();
+                    return false;
                 }
+
+                pstTransaccionDestino.setInt(1, transaccion.getCuentaDestino());
+                pstTransaccionDestino.setObject(2, transaccion.getNum_cuenta() == 0 ? null : transaccion.getNum_cuenta(), java.sql.Types.INTEGER);
+                pstTransaccionDestino.setString(3, transaccion.getTipo_entrega());
+                pstTransaccionDestino.setInt(4, transaccion.getMonto());
+                pstTransaccionDestino.setString(5, transaccion.getFecha());
+                pstTransaccionDestino.setObject(6, transaccion.getDescripcion() == null ? null : transaccion.getDescripcion(), java.sql.Types.VARCHAR);
+
+                int HistorialDestino = pstTransaccionDestino.executeUpdate();
+
+                if (HistorialDestino == 0) {
+                    cn.rollback();
+                    return false;
+                }
+
+                cn.commit();
+                return true;
+
+            }catch (SQLException ex){
+                cn.rollback();
+                System.err.println("Error en la transacción: " + ex.getMessage());
+                JOptionPane.showMessageDialog(null, "Fallo de transacción, llame a soporte.");
             }
-
-            cn.rollback();
-            return false;
-
         }catch (SQLException ex){
-            JOptionPane.showMessageDialog(null, "Fallo de transaccion");
-            System.out.println("Error en la transacción: " + ex.getMessage());
+            System.err.println("Error de conexión: " + ex.getMessage());
         }
         return false;
     }
